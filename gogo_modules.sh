1d5a7c31155490490306a45aa0a3a2de9dc6b3f0
John Russell <johncrussell25@gmail.com>
2020-05-06 17:36:46 -0400

    Downgraded some things to avoid a segfault with intel mpi

--- gogo_modules.sh
+++ gogo_modules.sh
@@ -2,8 +2,8 @@
 
 module load GCCcore/8.2.0 CMake/3.14.4
 
-module load intel/19.0.5-fasrc01 intel-mkl/2019.5.281-fasrc01 
+module load intel/18.0.5-fasrc01 intel-mkl/2019.5.281-fasrc01 
 
-module load impi/2019.5.281-fasrc01
+module load impi/2018.4.274-fasrc01
 
 
